332e7b123a54c3f7b4abc1cf8078c2664b7de63b
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-14 12:01:32 +0530

    terraform database mongodb inst

--- 5.databases/variable.tf
+++ 5.databases/variable.tf
@@ -15,3 +15,13 @@ variable "ec2_instance_pwd" {
   type = string
   sensitive = true
 }
+
+variable "zone_id" {
+  type = string
+  default = "Z00114331R4EBNTWP3VT8"
+}
+
+variable "domain_name" {
+  type = string
+  default = "manidevops.online"
+}
