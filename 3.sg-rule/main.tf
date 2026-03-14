70688b433b7960eebc0c76a3668ca2d12700686d
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-14 17:14:57 +0530

    mysql & redis sg rules added

--- 3.sg-rule/main.tf
+++ 3.sg-rule/main.tf
@@ -40,11 +40,32 @@ resource "aws_security_group_rule" "mongodb_user" {
   security_group_id = local.mongodb_sg_id
 }
 
-resource "aws_security_group_rule" "redis_bastion" { 
+resource "aws_security_group_rule" "redis_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = local.bastion_sg_id 
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
   security_group_id = local.redis_sg_id
+}
+
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mysql_sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.rabbitmq_sg_id
 }
