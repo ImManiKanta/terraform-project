5a41c43ea849d1e3580f8b20590febd15ed2ea6d
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-14 12:12:56 +0530

    databse inst

--- 5.databases/local.tf
+++ 5.databases/local.tf
@@ -1,5 +1,6 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
+  #database subnet in 1a AZ
   database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_id.value)[0]
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
