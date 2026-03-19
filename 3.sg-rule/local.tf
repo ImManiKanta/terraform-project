e445b144234775d1c16799e30811f810c5eb371e
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-19 19:46:22 +0530

    catalogue sg rule

--- 3.sg-rule/local.tf
+++ 3.sg-rule/local.tf
@@ -8,4 +8,9 @@ locals {
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   user_sg_id = data.aws_ssm_parameter.user_sg_id.value
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
 }
