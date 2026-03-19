4b83b3851265ec80f7ce4175404c384165c6a38a
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-19 18:57:52 +0530

    catalogue alg sg rule

--- 3.sg-rule/main.tf
+++ 3.sg-rule/main.tf
@@ -89,3 +89,13 @@ resource "aws_security_group_rule" "catalogue_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id = local.catalogue_sg_id
 }
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.backend_alb_sg_id
+  security_group_id = local.catalogue_sg_id
+}
