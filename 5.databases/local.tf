bfc74a5b02a9602c54c7acb6d6a1c11c86eaa10a
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-19 20:06:17 +0530

    local added

--- 5.databases/local.tf
+++ 5.databases/local.tf
@@ -6,4 +6,9 @@ locals {
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
 }
