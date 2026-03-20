6489d6f71a1f64a6f7049cac16ac4deeb9a9ff35
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-20 17:06:10 +0530

    catalogue config

--- 7.catalogue/variables.tf
+++ 7.catalogue/variables.tf
@@ -5,3 +5,13 @@ variable "project" {
 variable "environment" {
   default = "dev"
 }
+
+variable "zone_id" {
+  type = string
+  default = "Z00114331R4EBNTWP3VT8"
+}
+
+variable "domain_name" {
+  type = string
+  default = "manidevops.online"
+}
