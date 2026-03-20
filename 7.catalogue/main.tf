728e13def9b22b34b7225818316852a3c4b32e57
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-20 11:36:22 +0530

    catalogue instance

---
+++ 7.catalogue/main.tf
@@ -0,0 +1,41 @@
+#catalogue instance
+resource "aws_instance" "catalogue" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.private_subnet_id
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  tags = {
+    Name = "${var.project}-${var.environment}-catalogue"
+  }
+}
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+
+   provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+       "chmod +x /tmp/bootstrap.sh",
+       "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
+    ]
+  }
+}
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [terraform_data.catalogue]
+}
---
+++ 7.catalogue/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tf-remotestate-mani-dev"
+    key    = "terraform.tfstate-dev-catalogue"
+    region = "us-east-1"
+    use_lockfile = true # This is the default behavior now
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
