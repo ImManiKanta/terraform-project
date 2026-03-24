4fd61b5ab890f49ae80620f0c7aad631b6cc335b
ImManiKanta <chinthamanikanta1@gmail.com>
2026-03-24 15:11:14 +0530

    frontend alb changes

---
+++ 9.frontend-alb/main.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "frontend_alb" {
+  name               = "${var.project}-${var.environment}-frontend"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [local.frontend_alg_sg_id]
+  subnets            = [local.public_subnet_id]
+
+  enable_deletion_protection = false
+  
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-frontend"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_lb_listener" "frontend_alb" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = local.frontend_alb_certificate_arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "<h1>Hi, I am from HTTPS Frontend ALB</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = var.zone_id
+  name    = "*.${var.domain_name}"
+  type    = "A"
+  
+  # load balancer details
+  alias {
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+}
